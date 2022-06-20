9d42ecbb55e1a4668916cc4b53963a044a5ca698
Adarshraj Ravindran <adarshrajrav@gmail.com>
2022-06-20 14:36:43 -0700

    first commit. Have bare bones of project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  form = false;
+  data = [
+    { day: 'Wednesday', time: '5:00pm', temp: '90', wind: '5' },
+    { day: 'Tusday', time: '3:00pm', temp: '80', wind: '10' },
+    { day: 'Monday', time: '1:00pm', temp: '95', wind: '3' },
+    { day: 'Sunday', time: '10:00am', temp: '70', wind: '13' },
+    { day: 'Friday', time: '9:00pm', temp: '80', wind: '20' },
+];
+
 }
