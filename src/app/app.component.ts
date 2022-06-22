6647298871264fdeb85d976f8631750d735215d8
Adarshraj Ravindran <adarshrajrav@gmail.com>
2022-06-21 22:33:38 -0700

    added input validation and messages when input is invalid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,42 +10,63 @@ import { from } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  firstName="";
+  // used to load or not load certain elements
   form = false;
   sortMenu = false;
+
+  // default data for start
   data = [
-    { day: 'Wednesday', date: '2022-01-22', time: '5:00pm', temp: 90, wind: 5 },
-    { day: 'Tusday',  date: '2022-01-21', time: '3:00pm', temp: 80, wind: 10 },
-    { day: 'Monday',  date: '2022-01-20', time: '1:00pm', temp: 95, wind: 3 },
-    { day: 'Sunday',  date: '2022-01-19', time: '10:00am', temp: 70, wind: 13 },
-    { day: 'Friday',  date: '2022-01-24', time: '9:00pm', temp: 80, wind: 20 },
+    { date: '2022-01-22', time: '5:00pm', temp: 90, wind: 5 },
+    { date: '2022-01-21', time: '3:00pm', temp: 80, wind: 10 },
+    { date: '2022-01-20', time: '1:00pm', temp: 95, wind: 3 },
+    { date: '2022-01-19', time: '10:00am', temp: 70, wind: 13 },
+    { date: '2022-01-24', time: '9:00pm', temp: 80, wind: 20 },
   ];
-  selectedDay = '';
-  days = [
-    { id: 1, day: "Monday" },
-    { id: 2, day: "Tuesday" },
-    { id: 3, day: "Wednesday" },
-    { id: 4, day: "Thursday" },
-    { id: 5, day: "Friday" },
-    { id: 6, day: "Saturday" },
-    { id: 7, day: "Sunday" }
-  ];
+
+  // variables for user input for new data
   time='';
   date='';
   temp = 0;
   wind = 0;
+  // order is for ascending or descending
+  sortBy = ['Date', 'Temperature', 'Wind Speed']
+  sort = '';
+  selectSort = true;
   order = true;
+  // if data is bad change value to false
+  checkDate = true;
+  checkTime = true;
+
+  // --------------User entry function-------------- //
+
   submit () {
-    let obj = {
-      day : this.selectedDay,
-      date: this.date,
-      time: this.time,
-      temp: this.temp,
-      wind: this.wind,
+    // check data using regex
+    let goodData = true;
+
+    // checking time
+    this.time = this.time.replace(/ /g, '');
+    this.time = this.time.replace(/ A/g, 'a');
+    this.time = this.time.replace(/ P/g, 'p');
+    this.time = this.time.replace(/ M/g, 'm');
+    // edge case that I sometimes mess up
+    if ((/^\d:\d\d[ap]m/.test(this.time))) {
+      this.time = '0' + this.time;
     }
-    console.log("My input: ", obj);
+    if(!(/^\d\d:\d\d[ap]m/.test(this.time))) {
+      this.checkTime = false
+      goodData = false;
+    }
+
+    // checking date
+    this.date = this.date.replace(/\//g, '-');
+    if(!(/^\d\d\d\d-\d\d-\d\d/.test(this.date))) {
+      this.checkDate = false;
+      goodData = false;
+    }
+
+    if (goodData) {
+ // data is good. Push it to array;
     this.data.push({
-      day : this.selectedDay,
       date: this.date,
       time: this.time,
       temp: this.temp,
@@ -53,8 +74,25 @@ export class AppComponent {
     });
     console.log("Data after submit: ", this.data);
     this.form = false;
+    } else {
+      console.log ('Data did not submit');
+    }
+
   };
 
+  // --------------SORT functions-------------- //
+  submitSort () {
+    this.selectSort = true;
+    if(this.sort === 'Date'){
+      this.sortArrDate();
+    } else if(this.sort === 'Temperature'){
+      this.sortArrTemp();
+    } else if(this.sort === 'Wind Speed'){
+      this.sortArrWind();
+    } else {
+      this.selectSort = false;
+    }
+  }
   sortArrDate () {
     for (let i = 0; i < this.data.length - 1; i++) {
       let obj = this.data[i];
